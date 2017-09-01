73478fc6621566a0034c9ec202824d90a5c18230
Paul Rochowski <Paul.Rochowski@uni-due.de>
2017-09-01 18:52:16 +0200

    # Change bootstrap version on used version in mir # begin to implement validation on local user registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  rForm: FormGroup;
+  post: any;                     // A property for our submitted form
+  localuser_identification: string = '';
+  localuser_name: string = '';
+  localuser_mail: string = '';
+  localuser_password: string = '';
+  localuser_password_repeat: string = '';
+
+  identificationAlert: string = 'This field is required';
+
+  constructor(private fb: FormBuilder) {
+
+    this.rForm = fb.group({
+      'localuser_identification': [null, Validators.required],
+      'localuser_name': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+      'localuser_mail': '',
+      'localuser_password': '',
+      'localuser_password_repeat': ''
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  addPost(post) {
+
+  }
 }
