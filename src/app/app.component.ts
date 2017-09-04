c0c368bbe2c035b3b2464c1713a99fb2039e9935
Paul Rochowski <Paul.Rochowski@uni-due.de>
2017-09-04 12:08:40 +0200

    # Translation prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {TranslateService} from 'ng2-translate';
+
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,22 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {
+  constructor(private translate: TranslateService) {
 
+    /*
+     * prototype
+     */
+    translate.addLangs(["de", "en"]);
+    translate.setDefaultLang("de");
+
+    /*
+     * get the current browser language
+     */
+    let browserlang = translate.getBrowserLang();
+
+    //translate.use(browserlang.match("/de|en") ? browserLang : "de");
+
+    translate.use("de");
   }
 
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import {HttpModule, Http} from '@angular/http';
 import {RouterModule} from '@angular/router';
-import {TranslateModule} from 'ng2-translate';
-
+import {TranslateModule, TranslateLoader, TranslateStaticLoader, MissingTranslationHandler} from 'ng2-translate';
 import {AppComponent} from './app.component';
 import {RegisterlocalComponent} from './registerlocal/registerlocal.component';
 
+export function createTranslateLoader(http: Http) {
+  return new TranslateStaticLoader(http, './assets/i18n', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -26,6 +28,12 @@ import {RegisterlocalComponent} from './registerlocal/registerlocal.component';
       {path: '', redirectTo: 'new-author', pathMatch: 'full'},
       {path: '**', redirectTo: 'new-author', pathMatch: 'full'}
     ]),
+
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [Http]
+    })
 
   ],
   providers: [],
