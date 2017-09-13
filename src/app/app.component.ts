1a8596544d91e86067bd6ef3f0c0f49c74eef384
Paul Rochowski <Paul.Rochowski@uni-due.de>
2017-09-13 15:08:28 +0200

    # Define Route to registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
-
 
 @Component({
   selector: 'mir-app',
@@ -9,33 +7,9 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AppComponent {
 
-  constructor(private translate: TranslateService) {
-
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('de');
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    // /*
-    //  * prototype
-    //  */
-    // translate.addLangs(["de", "en"]);
-    // translate.setDefaultLang("de");
-    //
-    // /*
-    //  * get the current browser language
-    //  */
-    // let browserlang = translate.getBrowserLang();
-    //
-    // //translate.use(browserlang.match("/de|en") ? browserLang : "de");
-    //
-    // translate.use("de");
-  }
-
-  switchLanguage(language: string) {
-    this.translate.use(language);
+  constructor() {
   }
 
   ngOnInit() {
-
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 // import { NavigationComponent }  from './navigation/navigation.component';
 
@@ -9,7 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate: TranslateService) {
+
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('de');
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    // /*
+    //  * prototype
+    //  */
+    // translate.addLangs(["de", "en"]);
+    // translate.setDefaultLang("de");
+    //
+    // /*
+    //  * get the current browser language
+    //  */
+    // let browserlang = translate.getBrowserLang();
+    //
+    // //translate.use(browserlang.match("/de|en") ? browserLang : "de");
+    //
+    // translate.use("de");
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
 
   ngOnInit() {
   }
