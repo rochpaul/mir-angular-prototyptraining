cf60398946e4f57d94599ad1757343ae4b789a3c
Paul Rochowski <Paul.Rochowski@uni-due.de>
2017-09-26 11:49:53 +0200

    # check on slow performance issues

--- src/app/mir/navigation-builder/mcrmessages-manager/mcrmessages-manager.component.ts
+++ src/app/mir/navigation-builder/mcrmessages-manager/mcrmessages-manager.component.ts
@@ -30,6 +30,23 @@ export class MCRMessagesManagerComponent implements OnInit {
               private logger: NGXLogger,
               private mcrmessagesService: McrmessagesService) {
 
+    mcrmessagesService.getMessagesFromComponent().subscribe(mcrmessageKeys => {
+
+      // get form array
+      const mcrMessages = <FormArray>this.mcrmessagesForm.controls['mcrMessages'];
+
+      for (let mcrmessageKey of mcrmessageKeys) {
+
+        translateService.get('messages.' + mcrmessageKey).subscribe(
+          mcrMessageValue => {
+
+            logger.debug("MCRMessagesManager:: " + mcrmessageKey + ": " + mcrMessageValue);
+
+            //mcrMessages.push(this.createitems(new McrMessagesModel(mcrmessageKey, mcrMessageValue)));
+          }
+        )
+      }
+    });
   }
 
   ngOnInit() {
@@ -38,30 +55,11 @@ export class MCRMessagesManagerComponent implements OnInit {
     this.mcrmessagesForm = new FormGroup({
       mcrMessages: new FormArray([])
     });
-
-
-    this.mcrmessagesService.getMessagesFromComponent().subscribe(mcrmessageKeys => {
-
-      // get form array
-      const mcrMessages = <FormArray>this.mcrmessagesForm.controls['mcrMessages'];
-
-      for (let mcrmessageKey of mcrmessageKeys) {
-
-        this.translateService.get('messages.' + mcrmessageKey).subscribe(
-          mcrMessageValue => {
-
-            this.logger.debug("MCRMessagesManager:: " + mcrmessageKey + ": " + mcrMessageValue);
-
-            mcrMessages.push(this.createitems(new McrMessagesModel(mcrmessageKey, mcrMessageValue)));
-          }
-        )
-      }
-    });
   }
 
-  createitems(mcrmessage?: McrMessagesModel) {
-    return this.formBuilder.group({
-      mcrmessage: [mcrmessage]
-    });
-  }
+  // createitems(mcrmessage?: McrMessagesModel) {
+  //   return this.formBuilder.group({
+  //     mcrmessage: [mcrmessage]
+  //   });
+  // }
 }
